03707e14b2c7313155dc94d731947770cf9d45e9
lukasz-gabka <lukasz.gabka.programista@gmail.com>
2022-09-20 21:58:37 +0200

    added Image component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import './App.css';
+import './stylesheets/App.css';
+import { Container, Image, Row } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return ( 
-    <div></div>
+    <Container fluid>
+      <Row>
+        <Image src="images/fruits/apple.jpg" id='guessingImage' fluid />
+      </Row>
+    </Container>
   );
 }
 
